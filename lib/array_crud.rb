142a5293542edeed4016501dd2239a29e512db21
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-29 22:53:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,6 +29,7 @@ def remove_element_from_end_of_array(array)
 def remove_element_from_start_of_array(array)
   family = ["winifred", "Jasmine", "Kaleb", "Mackenzie"]
   winifred = family.shift
+  
 
 def retrieve_element_from_index(array, index_number)
   
