6cd1d6c12b8cb62ca839b1ff86c25933d1745d29
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-29 22:52:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,8 @@ def add_element_to_start_of_array(array, element)
 
 def remove_element_from_end_of_array(array)
   family = ["winifred", "Jasmine", "Kaleb", "Mackenzie"]
+  Mackenzie_family = family.pop
   
-end
 
 def remove_element_from_start_of_array(array)
   
