7da05f41dac4aa93014755325c13f810cbd3a8d4
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-29 22:44:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ end
 def create_an_array
   family = ["winifred", "Jasmine", "Kaleb", "Mackenzie"]
   
-end
+
 
 def add_element_to_end_of_array(array, element)
   array = ["winifred", "Jasmine", "Kaleb", "Mackenzie"]
@@ -14,7 +14,7 @@ def add_element_to_end_of_array(array, element)
   
 
   
-end
+
 
 def add_element_to_start_of_array(array, element)
   
