4e63f3ce3b1a0c53523b565ba697d05bf53ff47e
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-29 22:55:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,6 +39,7 @@ def retrieve_element_from_index(array, index_number)
 
 def retrieve_first_element_from_array(array)
   family = ["winifred", "Jasmine", "Kaleb", "Mackenzie"]
+  
 
 
 def retrieve_last_element_from_array(array)
